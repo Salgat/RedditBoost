612c638deadc1d69a5c0f13bbaf4ed74546a1263
Austin Salgat <salgat@salgat.net>
2015-12-07 20:39:09 -0600

    Refactor into a single function

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-console.log("RedditPlus Loaded");
-
 // Applies a CSS style rule to the provided stylesheet
-function applyStyleToSheet(stylesheet, css) {
+/*function applyStyleToSheet(stylesheet, css) {
 	if (style.styleSheet) {
 		style.styleSheet.cssText += css;
 	} else {
@@ -14,10 +12,10 @@ var css = "",
 	head = document.head || document.getElementsByTagName('head')[0],
 	style = document.createElement("style");
 style.type = 'text/css';
-head.appendChild(style);
+head.appendChild(style);*/
 
 ////////////////// Hover picture load //////////////////
-function createFloatingImage(href) {
+/*function createFloatingImage(href) {
 	$("body").append("<div class='image_hover' style='position:absolute; top: 0; left: 0;'><img src=" + href + " style='width: 10%;'></img>Test</div>");
 }
 
@@ -38,98 +36,95 @@ $("a").hover(
 			$(".image_hover").remove();
 		}
 	}
-);
+);*/
 
 ////////////////// User Comments Blocking //////////////////
-/*
-TODO:
-	- Nested comments aren't working right
-
-*/
 var bannedUsers = [];
 window.addEventListener("RetrievedObject", function(event) {
 	bannedUsers = event.detail["RedditPlus_BlockedUserForComments"];
-	console.log("Banned Users: " + bannedUsers);
   
 	// Go through each user and add a class to hide if matching
 	$( ".author" ).each(function( index ) {
 	  	var comment = $(this).parent().parent().parent();
 	  	if (bannedUsers != null && bannedUsers.indexOf($(this).text()) >= 0 && comment.attr("data-type") == "comment") {
-			console.log("Found match for user: " + $(this).text());
 			comment.removeClass("noncollapsed").addClass("collapsed");
 		}
 	});
  
 	// Add option to block or unblock a user
 	$("*[data-type='comment']").each(function( index ) {
-		console.log("Iterating through comment");
 		var user = $(this).children(".entry").children(".tagline").children(".author").text();
-		if (bannedUsers != null && bannedUsers.indexOf($(this).children(".entry").children(".tagline").children(".author").text()) >= 0) {
-			// Hide (not banned yet)
-			$(this).children(".entry").children(".tagline").append("<a href='javascript:void(0)' class='unblockUserComments' style='margin-left: 5px;' data-username='" + user + "'>" + "unhide user comments" + "</a>");
+		if (bannedUsers != null && bannedUsers.indexOf(user) >= 0) {
+			// Unhide
+			$(this).children(".entry").children(".tagline").append("<a href='javascript:void(0)' class='unblockUserComments' style='margin-left: 5px;' data-username='" + user + "'>" + "show user comments" + "</a>");
 		} else {
-			// Unhide (banned)
+			// Hide
 			$(this).children(".entry").children(".tagline").append("<a href='javascript:void(0)' class='blockUserComments' style='margin-left: 5px;' data-username='" + user + "'>" + "hide user comments" + "</a>");
 		}
 	});
-	
-	
 }, false);
 
 // Add click listeners for blocking users
 $(document).on("click", ".unblockUserComments", function() {
-	// Unblock the user
+	// Block the user
 	console.log("Unblocking user");
-	if (bannedUsers == null) bannedUsers = [];
-	
-	var userToUnban = $(this).attr("data-username");
-	var blockedIndex = bannedUsers.indexOf(userToUnban);
-	if (blockedIndex >= 0) {
-		bannedUsers.splice(blockedIndex, 1);
-		var bannedList = {}; 
-		bannedList["RedditPlus_BlockedUserForComments"] = bannedUsers;
-		window.dispatchEvent(new CustomEvent("StoreObject", { "detail": bannedList }));
-		
-		// Remove collapsed for all comments of this user
-		$("*[data-type='comment']").each(function( index ) {
-			if ($(this).children(".entry").children(".tagline").children(".author").text() == userToUnban) {
-				var textElement = $(this).children(".entry").children(".tagline").children(".unblockUserComments");
-				textElement.text("hide user comments");
-				textElement.removeClass("unblockUserComments").addClass("blockUserComments");
-				$(this).removeClass("collapsed").addClass("noncollapsed");
-			}
-		});
-	}
+	blockOrUnblockUserComments(this, bannedUsers, false);
 });
 
 // Add click listeners for unblocking users
 $(document).on("click", ".blockUserComments", function() {
 	// Unblock the user
 	console.log("Blocking user");
+	blockOrUnblockUserComments(this, bannedUsers, true);
+});
+
+
+function blockOrUnblockUserComments(thisElement, bannedUsers, isBlocking) {
+	var blockingClass;
+	if (!isBlocking) {
+		blockingClass = ".unblockUserComments";
+	} else {
+		blockingClass = ".blockUserComments";
+	}
+	var textHeadline;
+	if (!isBlocking) {
+		textHeadline = "hide user comments";
+	} else {
+		textHeadline = "show user comments";
+	}
+	
 	if (bannedUsers == null) bannedUsers = [];
 	
-	var userToUnban = $(this).attr("data-username");
-	var blockedIndex = bannedUsers.indexOf(userToUnban);
-	if (blockedIndex < 0) {
-		bannedUsers.push(userToUnban);
+	var userToChange = $(thisElement).attr("data-username");
+	var blockedIndex = bannedUsers.indexOf(userToChange);
+	if ((isBlocking && blockedIndex < 0) || (!isBlocking)) {
+		if (isBlocking) {
+			bannedUsers.push(userToChange);
+		} else {
+			bannedUsers.splice(blockedIndex, 1);
+		}
 		var bannedList = {}; 
 		bannedList["RedditPlus_BlockedUserForComments"] = bannedUsers;
 		window.dispatchEvent(new CustomEvent("StoreObject", { "detail": bannedList }));
 		
-		// Add collapsed for all comments of this user
-		$("*[data-type='comment']").each(function( index ) {
-			if ($(this).children(".entry").children(".tagline").children(".author").text() == userToUnban) {
-				var textElement = $(this).children(".entry").children(".tagline").children(".blockUserComments");
-				textElement.text("show user comments");
-				textElement.removeClass("blockUserComments").addClass("unblockUserComments");
-				$(this).removeClass("noncollapsed").addClass("collapsed");
+		$("*[data-type='comment']").each(function( index, thisComment ) {
+			if ($(thisComment).children(".entry").children(".tagline").children(".author").text() == userToChange) {
+				var textElement = $(thisComment).children(".entry").children(".tagline").children(blockingClass);
+				textElement.text(textHeadline);
+				
+				if (isBlocking) {
+					textElement.removeClass("blockUserComments").addClass("unblockUserComments");
+					$(thisComment).removeClass("noncollapsed").addClass("collapsed");
+				} else {
+					textElement.removeClass("unblockUserComments").addClass("blockUserComments");
+					$(thisComment).removeClass("collapsed").addClass("noncollapsed");
+				}
 			}
 		});
 	}
-});
+}
 
 $(document).ready(function() {
-    console.log("ready!");
 	// When page loads, initiate event to get banned users list and collapse all comments by those users
 	window.dispatchEvent(new CustomEvent("GetObject", { "detail": "RedditPlus_BlockedUserForComments"}));
 });
